215e49fa328e82fd2c473d21cf43964afee043da
RAG2007 <76128078+RAG2007@users.noreply.github.com>
2023-10-07 17:32:23 +0200

    even more random commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let x = 0;
 const open = ["Logo", "FirstButton", "SecondButton", "ThirdButton"];
-const page = ["MainPageHolder", "ReactionTimePageHolder", "SecondButton", "ThirdButton"];
+const page = ["MainPageHolder", "ReactionTimePageHolder", "WritingTestPageHolder", "ThirdButton"];
 for(let i = 0; i < 4; i++) {
 	document.getElementById(open[i]).addEventListener("click", ()=> {
 		document.getElementById(page[x]).classList.remove("open");
@@ -44,4 +44,75 @@ element.addEventListener("mousedown", (event)=> {
 		element.style.backgroundColor = "#010409";
 		element.innerHTML = `Click anywhere to start`;
 	}
+});
+const easy = "Lorem";
+let a = easy.split(' ');
+let all = 0;
+for(let i = 0; i < a.length; i++) {
+	let current = a[i];
+	const NewWord = document.createElement("word");
+	NewWord.id = `word${i}`;
+	NewWord.className = `word`;
+	document.getElementById("WritingTestPage").appendChild(NewWord);
+	for(let a = 0; a < current.length; a++) {
+		const NewLetter = document.createElement("letter");
+		NewLetter.id = `letter${all}`;
+		NewLetter.className = `letter`;
+		NewLetter.innerHTML = current[a];
+		document.getElementById(`word${i}`).appendChild(NewLetter);
+		all++;
+	}
+	if(i < a.length - 1) {
+		const NewLetter = document.createElement("letter");
+		NewLetter.id = `letter${all}`;
+		NewLetter.className = `letter`;
+		NewLetter.innerHTML = ` `;
+		document.getElementById(`word${i}`).appendChild(NewLetter);
+		all++;
+	}
+}
+let index = 0;
+let first = 0;
+let firstTime = 0;
+let done = 0;
+let nacc = 0;
+document.getElementById("WritingTestPageHolder").addEventListener("keydown", (event) => {
+	document.getElementById("WritingTestResults").style.display = "none";
+	document.getElementById("WritingTestPage").style.display = "block";
+	event.preventDefault();
+	if(done === 0) {
+		if (event.key === easy[index] ) {
+			if(first === 0) {
+				firstTime = Date.now();
+				first = 1;
+				done = 0;
+			}
+			if(index === easy.length - 1) {
+				const yourtime = Date.now() - firstTime;
+				document.getElementById("WritingTestResults").innerHTML = `Your typing speed is ${(a.length / (yourtime  / 1000) * 60).toFixed(2)} WPM with an accuracy of ${(easy.length - nacc) / easy.length * 100}%`;
+				document.getElementById("WritingTestResults").style.display = "block";
+				document.getElementById("WritingTestPage").style.display = "none";
+				index = 0;
+				first = 0;
+				done = 1;
+				nacc = 0;
+				for(let i = 0; i < easy.length; i++) {
+					document.getElementById(`letter${i}`).classList.remove(`letterdone`);
+				}
+				return;
+			}
+			document.getElementById(`letter${index}`).classList.add(`letterdone`);
+			let x = window.getSelection().getRangeAt(0);
+			if(index + 1 < easy.length) {
+				x.setStart(document.getElementById(`letter${index + 1}`), 0);
+				x.setEnd(document.getElementById(`letter${index + 1}`), 0);
+			}
+			index++;
+		} else {
+			if(((event.key >= 'A' && event.key <= 'z') || event.key === '.' || event.key === ',' || event.key === ':' || event.key === ';') && !event.shiftKey) {
+				nacc++;
+			}
+		}
+	}
+	done = 0;
 });
